659d047d19f3b436344237494c2934d673155572
taru-boy <tarutaru.com.5021@gmail.com>
2025-01-01 16:06:43 +0900

    day24 insta_api_1

--- insta-opning-copy.py
+++ insta-opning-copy.py
@@ -5,44 +5,61 @@ import pandas as pd
 import os
 import sys
 import re
+from datetime import datetime as dt
+from dotenv import load_dotenv
+from pprint import pprint
 
-"""
-メイン関数
-"""
 
-# .envファイルからトークン情報などの取得
+def main():
+    """
+    メイン関数
+    """
 
-# os.environ.getまたはos.getenvを使用してenvファイルからトークン情報の読み込み
+    # .envファイルからトークン情報などの取得
+    load_dotenv()
 
-# ユーザーIDを入力
+    # os.environ.getまたはos.getenvを使用してenvファイルからトークン情報の読み込み
+    access_token = os.getenv("ACCESS_TOKEN")
+    print(access_token)
+    version = os.getenv("VERSION")
+    print(version)
+    ig_user_id = os.getenv("IG_USER_ID")
+    print(ig_user_id)
 
-# 今日の日付を取得　文字列で年-月-日の型式にする
+    # ユーザーIDを入力
+    user_id = "moriyamakaede"
 
-# ユーザーIDを使ってビジネスディスカバリー情報の取得
+    # 今日の日付を取得　文字列で年-月-日の型式にする
+    today = dt.now().strftime("%Y-%m-%d")
 
-# API制限に引っかかった場合の処理　account_dict['error']['code'] == 4となる場合
+    # ユーザーIDを使ってビジネスディスカバリー情報の取得
+    account_dict = call_business_profile(version, ig_user_id, user_id, access_token)
+    pprint(account_dict["business_discovery"]["media"]["paging"]["cursors"].keys())
 
-# 取得した情報をjson_normalizeで一気にデータフレーム型式に変換
+    # API制限に引っかかった場合の処理　account_dict['error']['code'] == 4となる場合
 
-# 重複したカラム名があるとデータポータルで読み込めないため、重複しているカラム名idの左側のほうを残して右側は削除
+    # 取得した情報をjson_normalizeで一気にデータフレーム型式に変換
 
-# メディア情報の取り出し
+    # 重複したカラム名があるとデータポータルで読み込めないため、重複しているカラム名idの左側のほうを残して右側は削除
 
-# データフレームを作るための空の辞書を作成
+    # メディア情報の取り出し
 
-# after_keyがあれば、追加でデータを取得
+    # データフレームを作るための空の辞書を作成
 
-# after_keyがある場合
+    # after_keyがあれば、追加でデータを取得
 
-# 追加でデータを取得する
+    # after_keyがある場合
 
-# concatを使って先に作成したデータフレームと結合する
+    # 追加でデータを取得する
 
-# インデックスを振りなおす
+    # concatを使って先に作成したデータフレームと結合する
 
-# 結果csv保存用のディレクトリ作成
+    # インデックスを振りなおす
 
-# after_keyがない場合　そのままデータフレームを作成
+    # 結果csv保存用のディレクトリ作成
+
+    # after_keyがない場合　そのままデータフレームを作成
+
 
 """
 結果データ保存のための
@@ -58,24 +75,58 @@ import re
 
 # データフレームにするカラム名をキーとして、空のリストで初期化
 
-"""
-ビジネスディスカバリーで
-アカウントのプロフィール情報を取得
-"""
 
-# ビジネスディスカバリーのエンドポイントの設定　"https://graph.facebook.com/v9/ig_user_id?fields=business_discovery.username(user_id)){followers_count,media_count,media{comments_count,like_count}}&access_token={access-token}"
+def call_business_profile(
+    version: str, ig_user_id: str, user_id: str, access_token: str
+) -> dict:
+    """
+    ビジネスディスカバリーでアカウントのプロフィール情報を取得
 
-# GETリクエスト
+    Parameters
+    ----------
+    version : str
+        APIのバージョン番号（例: 'v9'）
+    ig_user_id : str
+        InstagramビジネスアカウントのユーザーID
+    user_id : str
+        取得対象のユーザーのInstagramユーザー名
+    access_token : str
+        Facebook Graph APIのアクセストークン
 
-# JSON文字列を辞書に変換
+    Returns
+    -------
+    dict
+        指定されたInstagramアカウントのプロフィール情報を含む辞書
+    """
 
-"""
-ページ送りのafter_keyを取得する
-"""
+    # ビジネスディスカバリーのエンドポイントの設定　"https://graph.facebook.com/v9/ig_user_id?fields=business_discovery.username(user_id)){followers_count,media_count,media{comments_count,like_count}}&access_token={access-token}"
+    business_api = f"https://graph.facebook.com/{version}/{ig_user_id}?fields=business_discovery.username({user_id}){{username, website, name, id, profile_picture_url, biography, follows_count, followers_count, media_count, media{{timestamp, like_count, comment_count, caption, media_url}}}}&access_token={access_token}"
 
-# after_keyがある場合
+    # GETリクエスト
+    r = requests.get(business_api)
 
-# after_keyがない場合
+    # JSON文字列を辞書に変換
+    account_dict = json.loads(r.content)
+    return account_dict
+
+
+def get_after_key(account_dict: dict) -> str:
+    """
+    ページ送りのafter_keyを取得する
+    """
+    after_key = ""
+    # after_keyがある場合
+    try:
+        after_key = account_dict["business_discovery"]["media"]["paging"]["cursors"][
+            "after"
+        ]
+        return after_key
+
+    # after_keyがない場合
+    except KeyError as e:
+        print("after_key", e)
+        return after_key
+
 
 """
 ユーザー名とafter_keyを受け取り、追加データ分を
@@ -100,3 +151,6 @@ try-exceptでエラー処理を記述
 # まず要素を取り出す media_url、caption、hash_tags、timestamp、like_count、comments_count
 
 # data_dictの各リストにappendで要素を入れていく
+
+if __name__ == "__main__":
+    main()
